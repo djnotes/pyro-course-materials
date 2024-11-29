667644300281025a57d7aa680e5cfd4c2ba43729
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2024-11-29 12:11:32 +0330

    Revert "Add delete and update functionality" screen didn't record
    This reverts commit 007543797955c2ad114d6d6c84c86dbd557a7d47.

--- db-support/final/myhandler.py
+++ db-support/final/myhandler.py
@@ -3,10 +3,9 @@ from pyrogram.types import Message
 from sqlalchemy import select
 
 from db import Note, get_db
-from util import selectNotes
 
 async def handle_updates(client: Client, message: Message):
-    parts = message.text.split(maxsplit=2)
+    parts = message.text.split(maxsplit=1)
     cmd = parts[0]
     match cmd:
         # case "/wind":
@@ -19,36 +18,21 @@ async def handle_updates(client: Client, message: Message):
         #     await message.reply_photo(photo = "media/water.jpg", reply_to_message_id=message.id)
         case "/add":
             session = get_db()
-            session.add(Note(author_id = message.from_user.id, title = parts[1] , text = parts[2]))
+            session.add(Note(author_id = message.from_user.id, text = parts[1]))
             session.commit()
             await message.reply("Note added")
         case "/note":
             session = get_db()
-            # stmt = select(Note).where(Note.author_id == message.from_user.id)
-            # notesText = ""
-            # for note in session.scalars(stmt):
-            #     notesText += note.text + '\n'
-            notesText = selectNotes(session, message.from_user.id)
+            stmt = select(Note).where(Note.author_id == message.from_user.id)
+            notesText = ""
+            for note in session.scalars(stmt):
+                notesText += note.text + '\n'
             await message.reply(notesText)
             
         case "/update":
-            session = get_db()
-            stmt = select(Note).where(Note.title == parts[1])
-            note = session.scalars(stmt).one()
-            note.text = parts[2]
-            session.commit()
-            notesText = selectNotes(session, message.from_user.id)
-            await message.reply(notesText)
-            
+            pass
         case "/delete":
-            session = get_db()
-            stmt = select(Note).where(Note.title == parts[1])
-            note = session.scalars(stmt).one()
-            session.delete(note)
-            session.commit()
-            notesText = selectNotes(session, message.from_user.id)
-            await message.reply(notesText)
+            pass
         
         case _:
             await message.reply(text = "Command not recognized")
-
