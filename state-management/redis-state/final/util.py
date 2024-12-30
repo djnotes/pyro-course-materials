c09c14c3c390492654e166e926edd884298baaf2
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2024-12-31 01:48:06 +0330

    create redis cache

---
+++ state-management/redis-state/final/util.py
@@ -0,0 +1,8 @@
+class Keys:
+    STATE = "_state_"
+    USERNAME = "_username_"
+    PASSWORD = "_password_"
+
+class Values:
+    ENTER_USERNAME = "_enter_username_"
+    ENTER_PASSWORD = "_enter_password_"
