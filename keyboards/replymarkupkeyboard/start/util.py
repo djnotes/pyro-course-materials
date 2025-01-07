a41dc045bdbf9b30387d4883ed8e9280e27bf8d6
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2025-01-07 23:55:30 +0330

    Recod replykeyboardmarkup

---
+++ keyboards/replymarkupkeyboard/start/util.py
@@ -0,0 +1,21 @@
+
+class Buttons:
+    home = "Home 🏠"
+    settings = "Settings ⚙️"
+    admins = "Admins 👥"
+    channels = "Channels 📺📺"
+    translations = "Languages 📙"
+    files = "Files 📁"
+    stats = "Stats 📊"  
+    info = "Info ℹ️"
+    language = "Language 📚"
+    add_admin = "Add Admin"
+    view_admins = "View Admins"
+    remove_admin = "Remove Admin"
+    remove_keyboard = "Remove Keyboard"
+    
+    
+    
+
+
+    
