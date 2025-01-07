5610a864ba3e0a493ccf258a8a2b718694147ff2
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2025-01-08 00:55:21 +0330

    Record inline keyboard query

---
+++ keyboards/inline-kb-query/final/util.py
@@ -0,0 +1,50 @@
+from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+class Buttons:
+    home = "Home 🏠"
+    settings = "Settings ⚙️"
+    admins = "Admins 👥"
+    channels = "Channels 📺📺"
+    translations = "Languages 📙"
+    files = "Files 📁"
+    stats = "Stats 📊"  
+    info = "Info ℹ️"
+    language = "Language 📚"
+    add_admin = "Add Admin"
+    view_admins = "View Admins"
+    remove_admin = "Remove Admin"
+    remove_keyboard = "Remove Keyboard"
+    make_inline_links = "Make Inline Links"
+    
+class Values:
+    CONFIRM_KB_REMOVAL = "_confirm_kb_removal_"
+    CANCEL_KB_REMOVAL = "_cancel_kb_removal_"
+    
+class Keyboards:
+    MainMenu = ReplyKeyboardMarkup(
+        [
+            [KeyboardButton(Buttons.settings)],
+            [KeyboardButton(Buttons.admins), KeyboardButton(Buttons.channels)],
+            [KeyboardButton(Buttons.make_inline_links),KeyboardButton(Buttons.remove_keyboard)]
+        ]
+    )
+
+    SettingsMenu = ReplyKeyboardMarkup(
+        [
+            [Buttons.language],
+            [KeyboardButton(Buttons.home)]
+        ]
+    )
+
+    AdminsMenu = ReplyKeyboardMarkup (
+        [
+            [Buttons.add_admin, Buttons.remove_admin],
+            [Buttons.view_admins],
+            [KeyboardButton(Buttons.home)]
+        ]
+    )
+    
+    
+
+
+    
