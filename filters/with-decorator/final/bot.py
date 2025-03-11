a240c43ea4f639657d0f1a6c4c34f068bd851de0
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2025-03-12 02:56:12 +0330

    Record advanced filters

--- filters/with-decorator/final/bot.py
+++ filters/with-decorator/final/bot.py
@@ -2,7 +2,7 @@ from pyrogram import Client, idle
 from pyrogram.types import Message
 from pyrogram import filters
 from pyrogram.handlers import MessageHandler
-from handlers import handle_audio, handle_visual, handle_audio_in_group
+from handlers import handle_audio, handle_visual, handle_audio_in_group, handle_start, handle_stats_cmd
 
 import os
 
@@ -24,6 +24,17 @@ app.add_handler(MessageHandler(handle_audio, filters.audio & filters.caption))
 app.add_handler(MessageHandler(handle_visual, filters.photo | filters.video | filters.animation))
 
 app.add_handler(MessageHandler(handle_audio_in_group, filters.audio & ~filters.private))
+
+app.add_handler(MessageHandler(handle_start, filters.command("start")))
+
+
+app.add_handler(MessageHandler(handle_stats_cmd, filters.command("stats")))
+
+#+18001231234
+@app.on_message(filters.regex(r"\+?\d{11}"))
+async def handle_phone_number(client, message):
+    await message.reply("You sent me a phone number")
+
 
 
 app.start()
--- filters/with-decorator/final/handlers.py
+++ filters/with-decorator/final/handlers.py
@@ -6,3 +6,10 @@ async def handle_visual(client, message):
 
 async def handle_audio_in_group(client, message):
     await message.reply("You sent me an audio in group")
+
+async def handle_start(client, message):
+    await message.reply("Bot started")
+    pass
+    
+async def handle_stats_cmd(client, message):
+    await message.reply("Stats:\nUsers: 110\nSubscriptions: 50")
